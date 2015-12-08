5099528e52a86c0cab3c504dd676b16a2aed5c2e
archiel45 <archiel.larot@gmail.com>
2015-12-08 17:34:07 +0800

    archiel edit

---
+++ application/models/users_model.php
@@ -0,0 +1,52 @@
+<?php
+class Users_Model extends CI_Model {
+
+        public function __construct()
+        {
+                $this->load->database();
+        }
+
+        public function get_users($id = FALSE)
+		{
+		        if ($id === FALSE)
+		        {
+		                $query = $this->db->get('user');
+		                return $query->result_array();
+		        }
+
+		        $query = $this->db->get_where('user', array('user_id' => $id));
+		        return $query->row_array();
+		}
+
+		public function record_count() {
+	        return $this->db->count_all("user");
+	    }
+
+	    public function fetch_users($limit, $start) {
+	        $this->db->limit($limit, $start);
+	        $this->db->order_by('stud_id','DESC');
+	        $query = $this->db->get("user");
+
+	        if ($query->num_rows() > 0) {
+	            foreach ($query->result() as $row) {
+	                $data[] = $row;
+	            }
+	            return $data;
+	        }
+	        return false;
+	   }
+
+	   public function delete_user($id)
+		{
+			$data = array(
+		        'status' => "Restore",
+		        
+		    );
+			 $this->db->where('user_id', $id);
+		    $this->db->update('user', $data);
+			
+			 redirect(base_url().'Users_Admin/index');
+		}
+
+		
+}
--- application/views/admin/header_content.php
+++ application/views/admin/header_content.php
@@ -7,7 +7,7 @@
 				  </div>
 					<nav id="top-menu">
 						<ul class="clearfix">
-							<li><a href="team.html">Users</a></li>
+							<li><a href="<?php echo base_url('users_admin/index');?>">Users</a></li>
 							<li><a href="team.html">Teams</a></li>
 							<li><a href="video.html">Games</a></li>
 							<li><a href="<?php echo base_url('news_admin');?>">News and Updates</a></li>
@@ -33,7 +33,7 @@
                    <nav class="mobilemenu">
                         <select>
                             <option value="<?php echo base_url('site/home');?>">Home</option>
-                            <option value="team.html">Users</option>
+                            <option value="<?php echo base_url('users_admin/index');?>">Users</option>
                             <option value="video.html">Teams</option>
                             <option value="<?php echo base_url('news_admin');?>">News and Updates</option>
                             <option value="<?php echo base_url('announcement_admin');?>">Announcements</option>
