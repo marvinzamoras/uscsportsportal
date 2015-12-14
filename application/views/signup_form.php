04f05864032fb6825a944b6ddcafc863d0993229
marvinzamoras <marvinzamoras@gmail.com>
2015-12-14 19:44:47 +0800

    added error validation

--- application/views/signup_form.php
+++ application/views/signup_form.php
@@ -10,9 +10,13 @@
 				echo form_input('lname',set_value('lname',''),'class="col-12" placeholder="Last Name" type="text"');
 				echo form_error('lname');
 				echo form_input('contact_no',set_value('contact_no',''),'class="col-12" placeholder="Contact No." type="text"');
+				echo form_error('contact_no');
 				echo form_input('email_address',set_value('email_address',''),'class="col-12" placeholder="Email Address" type="text"');
+				echo form_error('email_address');
 				echo form_input('home_address',set_value('home_address',''),'class="col-12" placeholder="Home Address" type="text"');
+				echo form_error('home_address');
 				echo form_input('school',set_value('school',''),'class="col-12" placeholder="School" type="text"');
+				echo form_error('school');
 				echo '<div class ="bottom-form">';
 				
 				echo '</div>';	
@@ -23,9 +27,13 @@
 			echo '<center><h4>LOGIN CREDENTIALS</h4></center>';
 			echo '<form>';
 				echo form_input('stud_id',set_value('stud_id',''),'class="col-12" placeholder="ID Number" type="text"');
+				echo form_error('stud_id');
 				echo form_input('username',set_value('username',''),'class="col-12" placeholder="Username" type="text"');
+				echo form_error('username');
 				echo form_password('password','','class="col-12" placeholder="Password" type="password"');
+				echo form_error('password');
 				echo form_password('password_confirm','','class="col-12" placeholder="Confirm Password" type="password"');
+				echo form_error('password_confirm');
 				echo '<div class ="bottom-form">';
 				echo '<center>';
 				echo '<br>';
