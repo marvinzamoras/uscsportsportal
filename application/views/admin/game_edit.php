c63d85fae116bf33a54f8ceff2d33b60cf949196
marvinzamoras <marvinzamoras@gmail.com>
2016-03-11 16:49:10 +0800

    edited validation for games

--- application/views/admin/game_edit.php
+++ application/views/admin/game_edit.php
@@ -21,17 +21,20 @@
 						
 							<br/>
 						    <label for="title">GAME NAME :</label>	
+						    
 						    <input type="text" name="game_name" value = "<?php echo set_value('game_name',$game_item['game_name']) ;?>" size="50"  class="focus"/><br/>
+						    <?php echo form_error('game_name');?>
 						    <!--<center><?php echo form_error('game_name'); ?></center>-->
 						    <br/>
 						    <br/>
 						<center>
 							<textarea name="game_desc" class= "textarea" id = "mytextarea"><?php echo $game_item['game_desc'];?></textarea><br/></br>
 						 	<center><?php echo form_error('game_desc'); ?></center>
-							<?php echo form_hidden('game_id',$game_item['game_id']); ?>
+							
+							<input type="hidden" name="game_id" value="<?php echo $game_item['game_id']?>" />
 						   	<input class="EButton" type="submit" name="submit" value="Post" onclick = "javascript:return confirm('Are you sure you want to save this?');"/>
 						</center>
-						<?php echo form_hidden('game_id', ''); ?>
+						
 						
 					<?php echo form_close(); ?>
 
